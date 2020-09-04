af3fa80308101137cb25ca2b491f04db6b598f4d
Alex Kessinger <alex@smartystreets.com>
2020-09-04 16:11:53 -0600

    should work

--- queue.js
+++ queue.js
@@ -1,6 +1,30 @@
 class Queue {
+	constructor() {
+		this.listSize = 0;
+		this.queue = [];
+	}
+
 	push(item) {
-		return item;
+		this.queue[this.listSize++] = item;
+	}
+
+	shiftQueue() {
+		let tempQueue = [];
+
+		for(let i = 0; i < this.listSize; i++) {
+			tempQueue[i] = this.queue[i+1];
+		}
+
+		this.queue = tempQueue;
+	}
+
+	pop() {
+		if (this.listSize <= 0) return "ERROR";
+		const returnItem = this.queue[0];
+		this.listSize--;
+		this.shiftQueue();
+
+		return returnItem;
 	}
 }
 
--- queue.test.js
+++ queue.test.js
@@ -1,7 +1,87 @@
 const Queue = require('./queue.js');
-const list = new Queue();
+let list = new Queue();
 
+afterEach(() => {
+	list = new Queue();
+});
 
 test('adds one item to list', () => {
-	expect(list.push(1)).toBe(1);
+	list.push(1);
+	expect(list.queue.length).toBe(1);
+});
+
+test('get list size when list is 0', () => {
+	expect(list.listSize).toBe(0);
+});
+
+test('when adding an item to the queue the size increases', () => {
+	list.push(1);
+	expect(list.listSize).toBe(1);
+});
+
+test("remove something from the list without anything in the list", () => {
+	expect(list.pop()).toBe("ERROR");
+});
+
+test("push two things off and pop one thing off", () => {
+	list.push(1);
+	list.push(1);
+	list.pop();
+	expect(list.listSize).toBe(1);
+});
+
+test("push one thing and pop two things", () => {
+	list.push(1);
+	list.pop();
+	expect(list.pop()).toBe("ERROR");
+});
+
+test("pop returns a value", () => {
+	list.push(1);
+	expect(list.pop()).toBe(1);
+});
+
+test("push two items and pop returns the last item pushed", () => {
+	list.push("first");
+	list.push("second");
+	expect(list.pop()).toBe("first");
+});
+
+test("push two items and pop two items", () => {
+	list.push("first");
+	list.push("second");
+	list.pop();
+	expect(list.pop()).toBe("second");
+});
+
+test("push three items and pop three items", () => {
+	list.push("first");
+	list.push("second");
+	list.push("third");
+	list.pop();
+	list.pop();
+	expect(list.pop()).toBe("third");
+});
+
+test("push three and pop 4", () => {
+	list.push("first");
+	list.push("second");
+	list.push("third");
+	list.pop();
+	list.pop();
+	list.pop();
+	expect(list.pop()).toBe("ERROR");
+});
+
+test("queue length test", () => {
+	list.push("first");
+	list.push("second");
+	list.push("third");
+	expect(list.listSize).toBe(3);
+	expect(list.queue.length).toBe(3);
+	list.pop();
+	list.pop();
+	expect(list.listSize).toBe(1);
+	expect(list.queue.length).toBe(1);
+
 });
